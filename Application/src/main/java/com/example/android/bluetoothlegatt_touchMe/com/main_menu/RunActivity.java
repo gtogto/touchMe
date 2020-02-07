11d22db63815a69605cdcc182749a3118bf361b0
gtogto <calicema59@gmail.com>
2020-02-07 17:40:29 +0900

    clicked event in RUN activity v1.0

--- Application/src/main/java/com/example/android/bluetoothlegatt_touchMe/com/main_menu/RunActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt_touchMe/com/main_menu/RunActivity.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,13 +31,20 @@ public class RunActivity extends Activity {
     private BluetoothLeService mBluetoothLeService;
     private String mDeviceAddress;
 
+    public static ImageView node_imageView01, node_imageView02, node_imageView03, node_imageView04, node_imageView05, node_imageView06;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.run_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        //final ImageView back_btn = (ImageView) findViewById(R.id.back_btn);
+        node_imageView01 = (ImageView) findViewById(R.id.node1);
+        node_imageView02 = (ImageView) findViewById(R.id.node2);
+        node_imageView03 = (ImageView) findViewById(R.id.node3);
+        node_imageView04 = (ImageView) findViewById(R.id.node4);
+        node_imageView05 = (ImageView) findViewById(R.id.node5);
+        node_imageView06 = (ImageView) findViewById(R.id.node6);
 
     }
 
@@ -105,6 +114,57 @@ public class RunActivity extends Activity {
         tMsg.setGravity(Gravity.CENTER, 0, 0);
         LinearLayout view = (LinearLayout) tMsg.getView();
         tMsg.show();
+
+        String choice_node = hexToASCII(hex_value).substring(13,14);
+
+        asciiToHex(choice_node);       // is 1
+
+        String node1 = "1";
+        String node2 = "2";
+        String node3 = "3";
+        String node4 = "4";
+        String node5 = "5";
+        String node6 = "6";
+
+        //System.out.println(" choice num = " + choice_node);
+
+        if (choice_node.equals(node1)) {
+            node_imageView01.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 1 = " + node1 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView01.setImageResource(R.drawable.green_circle_button1);
+
+        if (choice_node.equals(node2)) {
+            node_imageView02.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 2 = " + node2 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView02.setImageResource(R.drawable.green_circle_button1);
+
+        if (choice_node.equals(node3)) {
+            node_imageView03.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 3 = " + node3 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView03.setImageResource(R.drawable.green_circle_button1);
+
+        if (choice_node.equals(node4)) {
+            node_imageView04.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 4 = " + node4 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView04.setImageResource(R.drawable.green_circle_button1);
+
+        if (choice_node.equals(node5)) {
+            node_imageView05.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 5 = " + node5 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView05.setImageResource(R.drawable.green_circle_button1);
+
+        if (choice_node.equals(node6)) {
+            node_imageView06.setImageResource(R.drawable.clicked_pink_circle_button);
+            System.out.println("node 6 = " + node6 + " " + " choice num = " + choice_node);
+        }
+        else node_imageView06.setImageResource(R.drawable.green_circle_button1);
+
+
 
         return sb.toString();
     }
